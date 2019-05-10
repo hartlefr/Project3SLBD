481ec405ec50f56478278632890cb0fa2152e050
Franz Hartleitner <franz.hartleitner@gmail.com>
2019-05-10 20:44:31 +0200

    finished1

--- Project3.R
+++ Project3.R
@@ -7,7 +7,7 @@ len = length(train)
 if(perc*len > 1){
 tmp = train[1:(perc*len)]
 tmp = sample(tmp)
-result = c(tmp, train[(perc*len+1):len])
+result = c(tmp, train[floor((perc*len+1)):len])
 return(result)
 }
 return(train)
@@ -15,9 +15,10 @@ return(train)
 
 
 
-lambda = c(seq(0,2,0.05))
-N = 300
+lambda = c(seq(0,0.7,0.05))
+N = 800
 P = 100
+ml = c(seq(0,0.5,0.05))
 set.seed(489)
 
 data = matrix( rnorm(N*P,mean=0,sd=1), N, P) 
@@ -28,30 +29,46 @@ b1 = c(rep(1, 5))
 b2 = c(rep(0, P-5))
 b = c(b1,b2)
 y = data%*%b
+ytrain = y[1:(N/2)]
 ytest = y[((N/2)+1):N]
-ml = c(seq(0,0.6,0.05))
+rep = 1
+
+
+for(re in 1:rep){
 
 result = rep(0,length(lambda))
+result1 = rep(0,length(lambda))
 
 for(i in ml){
-cvreg = cv.glmnet(data, perm(y,i), lambda = lambda, alpha = 1)
+#cvreg = cv.glmnet(data, perm(y,i), lambda = lambda, alpha = 1) #org
+cvreg = cv.glmnet(training, perm(ytrain,i), lambda = lambda, alpha = 1)
+ypred = predict(cvreg, rev(lambda), newx = test)
 
-result = rbind(result,cvreg$cvm)
+cvmsqnp = apply(ypred, 2, function(x) sum((ytest - x)^2)^(1/2))
+
+#result = rbind(result,cvreg$cvm) #org
+result = rbind(result,cvmsqnp) #org
 
 }
 #lasso.mod <- glmnet(data, perm(y, 0.3), alpha = 1, lambda = lambda)
 #lasso.pred <- predict(lasso.mod, lambda, newx = test)
-
 #sqe = mean((lasso.pred-ytest)^2)^(1/2)
-
-
 #mean(ytest-lasso.pred)
 
 result = result[2:(length(ml)+1),]
-hm = melt(result)
+result1 = result1 + result
+
+}
+
+#end here
+
+hm = melt(result1)
 
 hm[1]<-rep(ml,length(lambda))
 hm[,2]<-lambda[hm[,2]]
+hm[,2]<-sort(hm[,2], decreasing = TRUE)
+
+
 
 ggplot(hm, aes(Var1,Var2)) + geom_tile(aes(fill=value), colour = "white") +  scale_fill_gradient(low = "white", high = "steelblue") + labs(x = "Mislabeling", y = "lambda") 
 
